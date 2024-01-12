04eab0fdc320dc7b3fcf7070bf15fb829792742b
ShashankKasera <ShashankKasera98@gmail.com>
2024-01-12 15:35:44 +0530

    #25:remove unnecessary import from RegistrationActivity

--- authentication/src/main/java/com/example/authentication/registration/RegistrationActivity.kt
+++ authentication/src/main/java/com/example/authentication/registration/RegistrationActivity.kt
@@ -1,7 +1,6 @@
 package com.example.authentication.registration
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
